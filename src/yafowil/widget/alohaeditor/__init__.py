6786c41a8ea7ca65f7f6a580f36c13b7ae29f807
Robert Niederreiter <office@squarewave.at>
2012-06-25 17:20:18 +0200

    resources

--- src/yafowil/widget/alohaeditor/__init__.py
+++ src/yafowil/widget/alohaeditor/__init__.py
@@ -1,31 +1,47 @@
 import os
+from yafowil.base import factory
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
+js = [{
+    'resource': 'http://cdn.aloha-editor.org/current/lib/aloha.js',
+    'thirdparty': False,
+    'order': 20,
+}, {
+    'resource': 'widget.js',
+    'thirdparty': False,
+    'order': 21,
+}]
+
+css = [{
+    'resource': 'http://cdn.aloha-editor.org/current/css/aloha.css',
+    'thirdparty': False,
+    'order': 20,
+}, {
+    'resource': 'widget.css',
+    'thirdparty': False,
+    'order': 21,
+}]
 
 
 def register():
     import widget
+    factory.register_theme('default', 'yafowil.widget.alohaeditor',
+                           resourcedir, js=js, css=css)
 
+
+###############################################################################
+# XXX: outdated below
+###############################################################################
 
 def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+    return resourcedir
 
 
 def get_js():
-    return [
-        {'resource': 'http://cdn.aloha-editor.org/current/lib/aloha.js',
-         'thirdparty': False,
-         'order': 20,},
-        {'resource': 'widget.js',
-         'thirdparty': False,
-         'order': 21,}
-    ]
+    return js
 
 
 def get_css():
-    return [
-        {'resource': 'http://cdn.aloha-editor.org/current/css/aloha.css',
-         'thirdparty': False,
-         'order': 20,},
-        {'resource': 'widget.css',
-         'thirdparty': False,
-         'order': 21,},
-    ]
+    return css
